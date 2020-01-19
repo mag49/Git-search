ab4690c675a16ca17d57457ade831f5be8aaa5f7
Ngethe <ciru930@gmail.com>
2020-01-19 21:52:22 +0300

    created the function for finding username in git hub

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class ProfileService {
-
-  constructor() { }
+  private username:string;
+  private clientid="";
+  private clientsecret="";
+  constructor(private http:Http) { 
+    console.log("service is now ready!");
+    this.username = 'mag49';
+  }
+  getProfileInfo(){
+    return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret);
+  }
 }
