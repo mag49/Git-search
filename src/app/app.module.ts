4212a5ec38ec22720240bd6b9bccffc672353de7
Ngethe <ciru930@gmail.com>
2020-01-21 11:16:46 +0300

    added the Readme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from'@angular/forms';
-import {ProfileService} from '././services/profile.service';
+import {ProfileService} from './services/profile.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
